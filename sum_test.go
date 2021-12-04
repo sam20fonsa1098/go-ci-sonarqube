081c2c5f78aebdfd4e0457dff6d1aecc8fd8fee3
Samuel Cristo da Fonseca <samfonsa12345@gmail.com>
2021-12-04 14:17:07 -0400

    test: :white_check_mark: adding tests in math operations

--- sum_test.go
+++ sum_test.go
@@ -8,3 +8,24 @@ func TestSum(t *testing.T) {
 		t.Error("The result must be 5")
 	}
 }
+
+func TestSub(t *testing.T) {
+	result := sub(2, 3)
+	if result != -1 {
+		t.Error("The result must be -1")
+	}
+}
+
+func TestMut(t *testing.T) {
+	result := sum(2, 3)
+	if result != 6 {
+		t.Error("The result must be 6")
+	}
+}
+
+func TestDiv(t *testing.T) {
+	result := div(2, 3)
+	if result != 2/3 {
+		t.Error("The result is wrong")
+	}
+}
